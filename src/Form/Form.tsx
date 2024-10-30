46e1b5a50647ba4d604bad9a976bdcafa0c07f73
martha-555 <martagolov4ak@gmail.com>
2024-10-30 16:50:04 +0200

    email validation is not completed

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import classes from './styles.module.css'
 
 const Form = () => {
 
-  const [name, setName] = useState('');
+  const [userInfo, setUserInfo] = useState({
+name: '',
+email: '',
+password: ''
+  });
   const [errorMessage,setErrorMessage] = useState({
     name:'',
     email: '',
     password:''
   })
 
-  const regex = /^[^\s]+@[^\s@]+\.[^\s@]+$/ ;
-  console.log(regex.test('martagolov4ak@gmail.com'))
+
 
   const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
- const target = e.target.value
- setName(target)
-  }
-const handleEmail = () => {
-  
+ const target = e.target.value;
+ setUserInfo((prev) => ({
+  ...prev,
+  name: target
+ }))
+ const regex = /^[^\s\.\W\d]+\s*$/
+!regex.test(target) && setErrorMessage((prevValue) =>({ ...prevValue,
+  name: "Неправильно вказане ім'я"}))
 }
 
+const regex = /^\w[^\s@]+@[^\s@]+\.[^\d]+\w[^\s\.]+\s*$/ ;
+console.log(regex.test('martagolov4ak@gmail.c.om'))
+const handleEmail = () => {
+}
+
+const handlePassword = () => {
+
+}
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault()
   }
+  console.log(errorMessage)
+  useEffect(() => {
+},[errorMessage])
     return(
       <div>
       <form onSubmit={handleSubmit} className={classes.form} action="martagolov4ak@gmail.com">
-        <input onChange={handleName} value={name} type="text" placeholder="name" />
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password"/>
+        <input required onChange={handleName} value={userInfo.name} type="text" placeholder="name" />
+        <input required type="email" placeholder="email" />
+        <input required type="password" placeholder="password"/>
         <button className={classes.formButton} type='submit'>Submit</button>
       </form>
       </div>
