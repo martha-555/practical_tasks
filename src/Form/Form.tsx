a2c80a81716d767b648c55370525f33370926cfc
martha-555 <martagolov4ak@gmail.com>
2024-11-13 12:37:10 +0200

    manual validation

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,56 +1,157 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import classes from './styles.module.css'
 
 const Form = () => {
-
+  const ref = useRef<HTMLButtonElement>(null);
+  
   const [userInfo, setUserInfo] = useState({
-name: '',
-email: '',
-password: ''
-  });
-  const [errorMessage,setErrorMessage] = useState({
-    name:'',
+    name: '',
     email: '',
-    password:''
-  })
+    password: ''
+  });
+  
+  const [errorMessage, setErrorMessage] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
 
+  const handleName = (e: React.FormEvent<HTMLInputElement>) => {
+    const name = (e.target as HTMLInputElement).value;
+    const regex = /^[A-Za-z]+$/;
+    
+    if (regex.test(name)) {
+      setUserInfo((prev) => ({
+        ...prev,
+        name
+      }));
+      setErrorMessage((prev) => ({
+        ...prev,
+        name: ''
+      }));
+    }
+    
+    if (!regex.test(name)) {
+      setErrorMessage((prevValue) => ({
+        ...prevValue,
+        name: "Invalid name format"
+      }));
+      setUserInfo((prev) => ({
+        ...prev,
+        name: ''
+      }));
+    }
+  };
 
+  const handleEmail = (e: React.FormEvent<HTMLInputElement>) => {
+    const start = '[^\\.\\-]';
+    const prohibitedCharacters = '(?!.*\\.{2}|--|\\.-|\\-\\.)';
+    const localPart = "[a-zA-Z0-9._%+!$'*=+-]";
+    const beforeTheDomain = '(?![\\.\\-])';
+    const allowedInTheDomain = '[a-zA-Z0-9.-]';
+    
+    const validEmail = new RegExp(`^${start}${prohibitedCharacters}${localPart}+\\w@${beforeTheDomain}${allowedInTheDomain}+\\w\\.\\w{2,}$`);
+    const email = (e.target as HTMLInputElement).value;
 
-  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
- const target = e.target.value;
- setUserInfo((prev) => ({
-  ...prev,
-  name: target
- }))
- const regex = /^[^\s\.\W\d]+\s*$/
-!regex.test(target) && setErrorMessage((prevValue) =>({ ...prevValue,
-  name: "Неправильно вказане ім'я"}))
-}
+    if (validEmail.test(email)) {
+      setUserInfo((prev) => ({
+        ...prev,
+        email
+      }));
+      setErrorMessage((prev) => ({
+        ...prev,
+        email: ''
+      }));
+    }
 
-const regex = /^\w[^\s@]+@[^\s@]+\.[^\d]+\w[^\s\.]+\s*$/ ;
-console.log(regex.test('martagolov4ak@gmail.c.om'))
-const handleEmail = () => {
-}
+    if (!validEmail.test(email)) {
+      setErrorMessage((prev) => ({
+        ...prev,
+        email: 'Invalid email'
+      }));
+      setUserInfo((prev) => ({
+        ...prev,
+        email: ''
+      }));
+    }
+  };
 
-const handlePassword = () => {
+  const handlePassword = (e: React.FormEvent<HTMLInputElement>) => {
+    const regex = /^(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*\W)\S{5,}$/;
+    const password = (e.target as HTMLInputElement).value;
 
-}
+    if (regex.test(password)) {
+      setUserInfo((prev) => ({
+        ...prev,
+        password
+      }));
+      setErrorMessage((prev) => ({
+        ...prev,
+        password: ''
+      }));
+    }
+
+    if (!regex.test(password)) {
+      setUserInfo((prev) => ({
+        ...prev,
+        password: ''
+      }));
+      setErrorMessage((prev) => ({
+        ...prev,
+        password: 'The password is not secure'
+      }));
+    }
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault()
-  }
-  console.log(errorMessage)
-  useEffect(() => {
-},[errorMessage])
-    return(
-      <div>
-      <form onSubmit={handleSubmit} className={classes.form} action="martagolov4ak@gmail.com">
-        <input required onChange={handleName} value={userInfo.name} type="text" placeholder="name" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password"/>
-        <button className={classes.formButton} type='submit'>Submit</button>
-      </form>
-      </div>
-    )
-}
+    e.preventDefault();
+  };
 
-export default Form
+  useEffect(() => {
+    if (ref.current && userInfo.email && userInfo.name && userInfo.password) {
+      ref.current.disabled = false;
+    }
+  }, [userInfo]);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className={classes.form} action="martagolov4ak@gmail.com">
+        <input
+          required
+          className={userInfo.name && classes.valid || errorMessage.name && classes.invalid || ''}
+          onChange={handleName}
+          type="text"
+          placeholder="name"
+        />
+        <span>{errorMessage.name}</span>
+        <input
+          required
+          className={userInfo.email && classes.valid || errorMessage.email && classes.invalid || ''}
+          onChange={handleEmail}
+          type="email"
+          placeholder="email"
+        />
+        <span>{errorMessage.email}</span>
+        <input
+          required
+          className={userInfo.password && classes.valid || errorMessage.password && classes.invalid || ''}
+          onChange={handlePassword}
+          type="password"
+          placeholder="password"
+          maxLength={10}
+        />
+        <span>{errorMessage.password}</span>
+        <button
+          ref={ref}
+          disabled
+          className={ref.current?.disabled === false ? classes.formButton : classes.formButtonDisabled}
+          type='submit'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
